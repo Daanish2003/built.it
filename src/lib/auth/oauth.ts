cec300a33e82b0235b3616914714403229549af4
Daanish2003 <redchillies6786@gmail>
2024-07-02 13:14:42 +0530

    fixed url in production for login

--- src/lib/auth/oauth.ts
+++ src/lib/auth/oauth.ts
@@ -1,9 +1,11 @@
 import { GitHub, Google } from "arctic"
 
+const isProduction = process.env.NODE_ENV === "production"
+
 export const google = new Google(
     process.env.AUTH_GOOGLE_ID!,
     process.env.AUTH_GOOGLE_SECRET!,
-    process.env.NEXT_PUBLIC_BASE_URL + "/api/auth/callback/google"
+    isProduction ? process.env.PROD_NEXT_PUBLIC_BASE_URL + "/api/auth/callback/google" : process.env.NEXT_PUBLIC_BASE_URL + "/api/auth/callback/google"
 )
 
 export const github = new GitHub(
